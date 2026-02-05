a337ffa3d0975667f6eb2fb482aa84f692e6c7f7
ValiB16 <valdet.beka@icloud.com>
2026-02-05 23:20:29 +0100

    Update and rename LTAV to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Let’s Talk and Vote</h1>
+      <p>Dein Multi-Post-Feed erscheint hier bald!</p>
+    </div>
+  )
+}
